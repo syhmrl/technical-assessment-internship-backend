cce051e01519d0717a6d83cbe85d82082275da23
JeeShen <jason@mysoftinn.com>
2025-04-15 11:05:13 +0800

    feat: add the assessment

--- TechnicalAssesmentBackendDeveloper/Program.cs
+++ TechnicalAssesmentBackendDeveloper/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Item Manager!");
+
+        ItemManager manager = new ItemManager();
+
+        // Part One: Fix the NullReferenceException
+        // This will throw a NullReferenceException
+        manager.AddItem("Apple");
+        manager.AddItem("Banana");
+
+        manager.PrintAllItems();
+
+        // Part Two: Implement the RemoveItem method
+        manager.RemoveItem("Apple");
+
+        // Part Three: Introduce a Fruit class and use the ItemManager<Fruit> to add a few fruits and print them on the console.
+        // TODO: Implement this part three.
+
+        // Part Four (Bonus): Implement an interface IItemManager and make ItemManager implement it.
+        // TODO: Implement this part four.
+    }
+}
+
+public class ItemManager
+{
+    private List<string> items;
+
+    public void AddItem(string item)
+    {
+        items.Add(item);
+    }
+
+    public void PrintAllItems()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    // Part Two: Implement the RemoveItem method
+    // TODO: Implement this method
+    public void RemoveItem(string item)
+    {
+        throw new NotImplementedException("RemoveItem method is not implemented yet. Please remove this line and implement this method.");
+    }
+
+    public void ClearAllItems()
+    {
+        items = [];
+    }
+}
+
+public class ItemManager<T>
+{
+    private List<T> items;
+
+    public void AddItem(T item)
+    {
+        items.Add(item);
+    }
+
+    public void PrintAllItems()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void ClearAllItems()
+    {
+        items = [];
+    }
+}
